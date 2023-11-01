a7aba0035b35a65a18e06c75e9d9ea338c05129f
Nuria00004 <nuriarustambekova4@gmail.com>
2023-11-01 19:42:04 +0600

    my forth hw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback, useMemo } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState("");
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
+  const deleteUser = useCallback((id) => {
+    setUsers((prevUsers) => prevUsers.filter((user) => user.id !== id));
+    setCounter((prevCounter) => prevCounter + 1);
+  }, []);
+
+  const addUser = useCallback(() => {
+    if (newUser) {
+      const id = Math.floor(Math.random() * 1000);
+      setUsers((prevUsers) => [...prevUsers, { id, name: newUser }]);
+      setNewUser("");
+      setCounter((prevCounter) => prevCounter + 1);
+    }
+  }, [newUser]);
+
+  const userListMemo = useMemo(
+    () =>
+      users.map((user) => (
+        <div className="every-user" key={user.id}>
+          {user.name}
+          <button onClick={() => deleteUser(user.id)}>Удалить</button>
+        </div>
+      )),
+    [users, counter]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="all-list">
+      <h2>Список пользователей</h2>
+      {userListMemo}
+      <input
+        type="text"
+        placeholder="Имя пользователя"
+        value={newUser}
+        onChange={(e) => setNewUser(e.target.value)}
+      />
+      <button className="add" onClick={addUser}>Добавить</button>
     </div>
   );
-}
+};
 
-export default App;
+export default UserList;
